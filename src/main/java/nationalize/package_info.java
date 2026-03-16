b269d85cafb1961592026390ba9dc257d51f8edf
TatorjanTiborBalazs <tatorjan.tibor5@gmail.com>
2026-03-16 15:39:20 +0100

    docs: add package_info.java

---
+++ src/main/java/nationalize/package_info.java
@@ -0,0 +1,16 @@
+/**
+ * Package to predict the nationality of a person based on a last name. The
+ * current implementation uses the
+ * <a href="https://nationalize.io/">nationalize.io</a> API.
+ *
+ * <p>Example of use:
+ * {@snippet lang="java" :
+ * var client = NationalizeClient.newInstance();
+ * var nationality = client.getNationality("Torvalds");
+ * var mostLikelyCountry = nationality.getMostLikelyCountry();
+ * }
+ */
+package nationalize;
+
+public class package_info {
+}
